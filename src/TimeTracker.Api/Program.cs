067845c6e93bf490909abfbc8fa0ae697297dd2d
Vladyslav Petrov <vladyslav.petrov@evinent.com>
2024-11-30 19:14:15 +0200

    Updated docker-compose

--- src/TimeTracker.Api/Program.cs
+++ src/TimeTracker.Api/Program.cs
@@ -10,6 +10,16 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 app.MapOpenApi();
